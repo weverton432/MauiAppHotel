b8144cd9d0f43df3919796c8b32adda33ce9143d
weverton432 <weverton.eleoterio271@gmail.com>
2025-05-29 08:53:28 -0300

    add recursos ao app

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -16,6 +16,7 @@ namespace MauiAppHotel
                     fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
                     fonts.AddFont("Kalam-Light.ttf", "KalamLight");
                     fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("RubikMoonrocks-Regular.ttf", "Rubik");
                 });
 
 #if DEBUG
